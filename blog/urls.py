ae1247c73ad8d8f87e93d44a6a0ed4559ecd1f5c
abhiabhi94 <abhy.abhy94@gmail.com>
2019-09-07 06:49:25 +0530

    Crispy form and users added to installed apps

--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from Users import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('Blog.urls')),
+    path('register/', user_view.register, name='Register'),
 ]
