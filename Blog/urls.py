892820d7fd14c37a980b45fb946953ac65539ae0
abhiabhi94 <abhy.abhy94@gmail.com>
2019-09-09 16:08:13 +0530

    Urls for login, logout, profile and register added

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from Users import views as user_views
 from . import views
 
 app_name = 'Blog'
@@ -7,5 +9,9 @@ app_name = 'Blog'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
-]
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='Users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='Users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
+    ]
 
