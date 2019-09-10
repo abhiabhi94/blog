c11c7295a02fddc3d697953e9acb501d19301ba8
abhiabhi94 <abhy.abhy94@gmail.com>
2019-09-10 07:28:44 +0530

    Slug field added to post model

--- Blog/models.py
+++ Blog/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.template.defaultfilters import slugify
+# from django.contrib.messages import messages
+import random
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=80, unique=True)
+    slug = models.SlugField(default='', max_length=80)
     content = models.TextField()
     tags = models.CharField(max_length=80, blank=True)
     date_posted = models.DateTimeField(default=timezone.now)
@@ -12,5 +17,12 @@ class Post(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     publish = models.BooleanField(default=False)
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(*args, **kwargs).save()
+
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('Blog:post-preview', kwargs={'slug':self.slug})
