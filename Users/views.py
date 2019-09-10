d1038e8ed92dd47a31009910ee45aedf6350caa6
abhiabhi94 <abhy.abhy94@gmail.com>
2019-09-11 01:29:54 +0530

    profile url updated

--- Users/views.py
+++ Users/views.py
@@ -25,7 +25,7 @@ def profile(request):
             u_form.save()
             p_form.save()
             messages.success(request, f'Your account has been updated!')
-            return redirect('Blog:profile')
+            return redirect('profile')
     
     else:
         u_form = UserUpdateForm(instance=request.user)
