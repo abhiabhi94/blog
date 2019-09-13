accd750f157c026c3a948311b1bb0ee0cc157bd4
abhiabhi94 <abhy.abhy94@gmail.com>
2019-09-13 23:43:35 +0530

    First name and last name field added to user registration and updation form

--- Users/forms.py
+++ Users/forms.py
@@ -4,18 +4,22 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100,required=False)
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name','last_name', 'email', 'password1', 'password2']
 
 class UserUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100, required=False)
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email']
 
 class ProfileUpdateForm(forms.ModelForm):
 
