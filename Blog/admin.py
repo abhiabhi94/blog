28cc5fe2dac4e53066e20b31809ac0e918ecfef0
abhiabhi94 <abhy.abhy94@gmail.com>
2019-09-14 04:09:42 +0530

    Admin page now displays last edited time of posts

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Post
-# Register your models here.
 
-admin.site.register(Post)
+class RatingAdmin(admin.ModelAdmin):
+    readonly_fields = ('last_updated',)
+
+admin.site.register(Post, RatingAdmin)
