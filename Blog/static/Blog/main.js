39f15d7aa497282f5cddc931b682afdad1cf36d0
abhiabhi94 <abhy.abhy94@gmail.com>
2019-09-19 06:31:14 +0530

    function to copy link is now working

--- Blog/static/Blog/main.js
+++ Blog/static/Blog/main.js
@@ -1,6 +1,32 @@
-function Copy(text) {
-    console.log('inside copy function');
-    console.log(text, 'fgdf');
-    $('.copy').select();
-    document.execCommand("copy");
-}
+$(document).ready(function() {
+    $('.copy').on('click', function(event) {
+        //Check if the request if for a blog or the window
+        if ($(this).data) {
+            var text = $(this).data('text');
+        } else {
+            var text = window.location.href;
+        }
+        console.log('link:', text);
+        var dummy = document.createElement('input');
+        $('body').append(dummy);
+        dummy.value = text;
+        dummy.select();
+        //For IE
+        if (window.clipboardData) {
+            window.clipboardData.setData('Text', text);
+        } else {
+            document.execCommand('copy');
+            event.preventDefault();
+        }
+        // dummy.value = 'Link copied successfully'
+        // dummy.dialog();
+        alert('Link copied successfully');
+        $('body').remove(dummy);
+    });
+    $('.bookmark').on('click', function(event) {
+        console.log('Bookmark button pressed');
+        var slug = $(this).data('post');
+        console.log(slug);
+        event.preventDefault();
+    });
+});
