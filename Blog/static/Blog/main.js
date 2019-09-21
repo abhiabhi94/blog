f0321f5368f19547826b16385849452ecf13e4dc
abhiabhi94 <abhy.abhy94@gmail.com>
2019-09-22 00:40:42 +0530

    Bookmark post removed if bookmarked

--- Blog/static/Blog/main.js
+++ Blog/static/Blog/main.js
@@ -19,23 +19,23 @@ $(document).ready(function() {
             document.execCommand('copy');
         }
         $(dummy).remove();
-        createResponse($(this), 'success', 'Link Copied Successfully');
+        createResponse('info', 'Link Copied Successfully');
     });
     $('.bookmark').on('click', function(event) {
         event.preventDefault();
         var post = $(this).data('post');
         url = 'post/bookmark';
-        sendAjax($(this), url, post);
+        sendAjax(url, post);
     });
 });
 
-function sendAjax(obj, link, data) {
+function sendAjax(link, data) {
     $.ajax({
         type: 'POST',
         headers: { 'X-CSRFToken': window.CSRF_TOKEN },
         url: link,
         contentType: 'application/json',
-        dataType: 'json',
+        // dataType: 'json',
         data: JSON.stringify({ 'data': data }),
         error: function(jqXhr, textStatus, errorMessage) {
             // console.log('Error Message:' + errorMessage);
@@ -50,17 +50,22 @@ function sendAjax(obj, link, data) {
             else
                 var state = 'warning';
             var msg = data.responseJSON['message'];
-            createResponse(obj, state, msg);
+            createResponse(state, msg);
         },
     });
 }
 
-function createResponse(obj, status, msg) {
-    var response = $('<div>').attr('id', 'response')
-        .addClass('alert alert-' + status)
-        .html('<div>' + msg + '</div')
-    obj.append(response);
+function createResponse(status, msg) {
+    let cls = 'alert alert-' + status;
+    var response = $('#response');
+    // var dummy = response.add('<div>');
+    // let temp = `<div class="alert ${cls}"` + `>${msg}</div>`;
+    var temp = $('<div/>')
+        .addClass(cls)
+        .html('<div>' + msg + '</div>');
+    // console.log(temp);
+    response.append(temp);
     setTimeout(function() {
-        $('#response').remove()
+        temp.remove();
     }, 5000);
 }
