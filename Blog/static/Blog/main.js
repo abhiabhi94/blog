526bba04d3812567e27aa5740ce86cfb0bd28b13
abhiabhi94 <abhy.abhy94@gmail.com>
2019-09-22 14:21:18 +0530

    Added functions for fixing the temp div to the top, fading it in and out slowly and handling exception for AJAX if user isn't logged in

--- Blog/static/Blog/main.js
+++ Blog/static/Blog/main.js
@@ -44,13 +44,18 @@ function sendAjax(link, data) {
             // console.log('status:' + status + ', data:' + data);
         },
         complete: function(data) {
-            // console.log(data.responseJSON['message']);
-            if (data.responseJSON['status'] === 0)
-                var state = 'success';
-            else
-                var state = 'warning';
-            var msg = data.responseJSON['message'];
-            createResponse(state, msg);
+            try {
+                // console.log(data.responseJSON['message']);
+                if (data.responseJSON['status'] === 0)
+                    var state = 'success';
+                else
+                    var state = 'warning';
+                var msg = data.responseJSON['message'];
+                createResponse(state, msg);
+            } catch (TypeError) {
+                top.location.href = '/login';
+                alert('fuck you krishna');
+            }
         },
     });
 }
@@ -58,14 +63,25 @@ function sendAjax(link, data) {
 function createResponse(status, msg) {
     let cls = 'alert alert-' + status;
     var response = $('#response');
-    // var dummy = response.add('<div>');
-    // let temp = `<div class="alert ${cls}"` + `>${msg}</div>`;
     var temp = $('<div/>')
         .addClass(cls)
         .html('<div>' + msg + '</div>');
     // console.log(temp);
     response.append(temp);
+    var time = 2000;
+    fixToTop(temp);
+    temp.fadeIn(time)
+    temp.fadeOut(2 * time);
     setTimeout(function() {
         temp.remove();
-    }, 5000);
+    }, 2 * time + 1000);
+}
+
+function fixToTop(div) {
+    var isfixed = div.css('position') == 'fixed'
+    if (div.scrollTop() > 200 && !isfixed)
+        div.css({ 'position': 'fixed', 'top': '0px' })
+    if (div.scrollTop < 200 && isfixed)
+        div.css({ 'position': 'static', 'top': '0px' })
+
 }
