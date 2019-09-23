d2aa93e4dc06b6db4509c3d2d62bde7da131ae3a
abhiabhi94 <abhy.abhy94@gmail.com>
2019-09-23 11:56:00 +0530

    only necessary functions are imported from the view

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,26 +2,28 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from Users import views as user_views
-from .views import (PostListView,
+from .views import (about,
+                    PostListView,
                     PostDetailView,
                     PostCreateView,
                     PostUpdateView,
                     PostDeleteView,
-                    UserPostListView
+                    UserPostListView,
+                    preview,
+                    bookmark_post,
                     )
-from . import views
 
 app_name = 'Blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),  
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
-    path('about', views.about, name='about'),
+    path('about', about, name='about'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/preview/<slug:slug>', views.preview, name='post-preview'),
+    path('post/preview/<slug:slug>', preview, name='post-preview'),
     path('post/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<slug:slug>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<slug:slug>/delete', PostDeleteView.as_view(), name='post-delete'),
-    path('post/bookmark', views.bookmark_post, name='bookmark-post'),
+    path('post/bookmark', bookmark_post, name='bookmark-post'),
     ]
 
