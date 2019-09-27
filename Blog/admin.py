c72660176c5a42c9ad7bf87c92b515e05892d446
abhiabhi94 <abhy.abhy94@gmail.com>
2019-09-27 14:20:33 +0530

    Filter for separating published and unpublished posts

--- Blog/admin.py
+++ Blog/admin.py
@@ -3,5 +3,6 @@ from .models import Post
 
 class RatingAdmin(admin.ModelAdmin):
     readonly_fields = ('last_updated', '_content_rendered')
+    list_filter = ['publish']
 
 admin.site.register(Post, RatingAdmin)
