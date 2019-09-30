532237ef4a703ecb3f4da924de273f67c117d343
abhiabhi94 <abhy.abhy94@gmail.com>
2019-09-30 17:59:36 +0530

    getting all tags and top tags from js

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,35 +2,26 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from Users import views as user_views
-from .views import (about,
-                    PostListView,
-                    PostDetailView,
-                    PostCreateView,
-                    PostUpdateView,
-                    PostDeleteView,
-                    UserPostListView,
-                    TaggedPostListView,
-                    preview,
-                    bookmark_post,
-                    UserPostBookmark,
-                    )
+from .import views
 from Track.views import hit_count
 
 
 app_name = 'Blog'
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='home'),  
-    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
-    path('user/<str:username>/bookmarks', UserPostBookmark.as_view(), name='user-bookmarks'),
-    path('<str:username>/posts', UserPostListView.as_view(), name='my-posts'),
-    path('about', about, name='about'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/preview/<slug:slug>', preview, name='post-preview'),
-    path('post/<slug:slug>/', hit_count(PostDetailView.as_view()), name='post-detail'),
-    path('post/<slug:slug>/update', PostUpdateView.as_view(), name='post-update'),
-    path('post/<slug:slug>/delete', PostDeleteView.as_view(), name='post-delete'),
-    path('post/tag/<str:tag>', TaggedPostListView.as_view(), name='tagged'),
-    path('post/bookmark', bookmark_post, name='bookmark-post'),
+    path('', views.PostListView.as_view(), name='home'),  
+    path('user/<str:username>', views.UserPostListView.as_view(), name='user-posts'),
+    path('user/<str:username>/bookmarks', views.UserPostBookmark.as_view(), name='user-bookmarks'),
+    path('<str:username>/posts', views.UserPostListView.as_view(), name='my-posts'),
+    path('about', views.about, name='about'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/preview/<slug:slug>', views.preview, name='post-preview'),
+    path('post/<slug:slug>/', hit_count(views.PostDetailView.as_view()), name='post-detail'),
+    path('post/<slug:slug>/update', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<slug:slug>/delete', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/tag/<str:tag>', views.TaggedPostListView.as_view(), name='tagged'),
+    path('post/bookmark', views.bookmark_post, name='bookmark-post'),
+    path('post/tag', views.get_tags, name='all-tags'),
+    path('post/top-tags', views.get_top_tags, name='top-tags'),
     ]
 
