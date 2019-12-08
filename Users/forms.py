e21d4f095d29ddd3ec7c17acd43948b38560001b
abhiabhi94 <abhy.abhy94@gmail.com>
2019-12-09 04:52:34 +0530

    facebook field removed from author's profile

--- Users/forms.py
+++ Users/forms.py
@@ -3,14 +3,17 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
+
 class UserRegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100,required=False)
+    last_name = forms.CharField(max_length=100, required=False)
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'first_name','last_name', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name',
+                  'email', 'password1', 'password2']
+
 
 class UserUpdateForm(forms.ModelForm):
     first_name = forms.CharField(max_length=100)
@@ -21,9 +24,11 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
 
+
 class ProfileUpdateForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-        fields = ['public', 'bio', 'image', 'website', 'location', 'facebook', 'twitter', 'instagram', 'linkedin']
-
+        fields = ['public', 'bio', 'image', 'website', 'location',
+                  #   'facebook',
+                  'twitter', 'instagram', 'linkedin']
