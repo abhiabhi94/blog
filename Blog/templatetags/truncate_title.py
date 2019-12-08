3f4933b9df8eb5a2de6f4d7d9f88d03038c37ba6
abhiabhi94 <abhy.abhy94@gmail.com>
2019-12-09 02:33:09 +0530

    tag to truncate characters in title

---
+++ Blog/templatetags/truncate_title.py
@@ -0,0 +1,29 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.template.defaultfilters import register
+
+register = template.Library()
+
+
+@register.filter(name='truncate_title', is_safe=True)
+@stringfilter
+def truncate_title(val, max_len):
+    '''
+    Returns a truncated_version of value after max_len.
+    If the last word doesn't completely fit inside max_len, it is truncated completely.
+    Adds ...(ellipsis character) at the end when truncation is done.
+    '''
+
+    try:
+        max_len = int(max_len)
+    except ValueError:
+        return max_len
+
+    if len(val) > max_len:
+        truncated_val = val[:max_len]
+        # Check if the next character is a space(This means the word ends here)
+        if val[max_len] is not ' ':
+            truncated_val = val[:val.rfind(' ')]
+        return truncated_val + '...'
+
+    return val
