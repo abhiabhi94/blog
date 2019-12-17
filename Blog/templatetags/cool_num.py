8155b10b46b54fe4bc77000ca2adad9c04365408
abhiabhi94 <abhy.abhy94@gmail.com>
2019-12-17 19:39:15 +0530

    Added tag for displaying views in cool format, fixed #81

---
+++ Blog/templatetags/cool_num.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='cool_num', is_safe=True)
+def cool_num(val, precision=2):
+    '''Convert numbers to a cool format e.g: 1K, 123.4K, 111.42M.'''
+
+    int_val = int(val)
+    if int_val < 1000:
+        return str(int_val)
+    elif int_val < 1_000_000:
+        return f'{ int_val/1000.0:.{precision}f}'.rstrip('0').rstrip('.') + 'K'
+    else:
+        return f'{int_val/1_000_000.0:.{precision}f}'.rstrip('0.') + 'M'
