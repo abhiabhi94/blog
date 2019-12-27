30c046ee1b8d93d947c42cf8237e93e197da73ca
abhiabhi94 <abhy.abhy94@gmail.com>
2019-12-27 06:06:21 +0530

    static file names will now be hashed on a production server, resolves #83

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -1,4 +1,26 @@
 from .base import *
+from subprocess import getoutput
+import urllib.request
+from urllib.error import URLError
 
 DEBUG = False
 ALLOWED_HOSTS = ['hackadda.com', ]
+
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+# IP_PRIVATE = getoutput('hostname -I').strip()
+# try:
+#     IP_PUBLIC = urllib.request.urlopen(
+#         'https://ident.me').read().decode('utf8')
+#     ALLOWED_HOSTS.append(IP_PUBLIC)
+
+#     print(
+#         f'You may connect using: {IP_PUBLIC}, {IP_PRIVATE}, localhost')
+
+# except URLError:
+#     print('Not connected to internet, the developement server will not be accessible from outside')
+
+# finally:
+#     ALLOWED_HOSTS.append(IP_PRIVATE)
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
