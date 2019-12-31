fc3979bd6c90636d1e06a6102bc4ca5c3115a2a8
abhiabhi94 <abhy.abhy94@gmail.com>
2020-01-01 02:29:20 +0530

    Meta caused error due to HEAD@{1}, now fixed

--- Users/views.py
+++ Users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from meta.views import Meta
 
 # Create your views here.
 
