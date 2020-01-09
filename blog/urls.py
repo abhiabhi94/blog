ff2ba0eb18e6138ac5bc0fc57ca100b26dda8351
abhiabhi94 <abhy.abhy94@gmail.com>
2020-01-10 05:29:12 +0530

    * Prevent authenticated users from entering in to the login page, via url --- fixed # 117, no message is displayed in this case.

--- blog/urls.py
+++ blog/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('', include('Blog.urls')),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='Users/login.html'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name='Users/login.html'),
          name='login'
          ),
     path('logout/', auth_views.LogoutView.as_view(
