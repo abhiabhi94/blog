da5d84e40913ff2bfe4684cfec5d847dc0c0fdea
abhiabhi94 <abhy.abhy94@gmail.com>
2020-01-10 21:10:27 +0530

    fixed syntax errors

--- Blog/templatetags/footer_social.py
+++ Blog/templatetags/footer_social.py
@@ -1,10 +1,12 @@
-from django.template.library import register
+from django import template
+
+register = template.Library()
 
 
 @register.inclusion_tag('footer_social.html', takes_context=True)
 def footer_socials(context):
     social
-    context{
+    context = {
         'footer_fb': 'http://facebook.com/thehackadda',
         'footer_twitter': 'https://twitter.com/thehackadda',
         'footer_ig': 'https://instagram.com/hackadda',
