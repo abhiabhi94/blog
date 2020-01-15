1afc6e1ae4f0f19045c312a8f0f803451995683a
abhiabhi94 <abhy.abhy94@gmail.com>
2020-01-15 05:37:15 +0530

    * Add author filter to admin * Resolves #123

--- Blog/admin.py
+++ Blog/admin.py
@@ -3,6 +3,7 @@ from .models import Post, Category
 from datetime import date
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
 
 class TagListFilter(admin.SimpleListFilter):
@@ -67,6 +68,40 @@ class CategoryListFilter(admin.SimpleListFilter):
             return queryset.filter()
 
 
+class AuthorListFilter(admin.SimpleListFilter):
+    title = _('Authors')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'author'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        author_list = User.objects.filter(
+            groups__name='Editors').values('id', 'username')
+        authors = tuple((author['id'], author['username'])
+                        for author in author_list)
+        return authors
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        if self.value():
+            post_list = Post.objects.filter(
+                author=self.value()).order_by('-date_created')
+            return post_list
+        else:
+            return queryset.filter()
+
+
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('slug', 'last_updated', 'views',
                        'date_published', 'thumbnail')
@@ -75,7 +110,8 @@ class PostAdmin(admin.ModelAdmin):
     # tags_list = [post.get_tags_list()
     #              for post in Post.objects.filter(publish=True)]
     # all_tags = list({item for outer in tags_list for item in outer})
-    list_filter = ['publish', 'featured', TagListFilter, CategoryListFilter]
+    list_filter = [AuthorListFilter, 'publish', 'featured',
+                   TagListFilter, CategoryListFilter]
 
 
 class CategoryAdmin(admin.ModelAdmin):
