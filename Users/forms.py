2585c028c19beba574747b82b2ef3fc4b8f21906
abhiabhi94 <abhy.abhy94@gmail.com>
2020-01-15 05:38:18 +0530

    * Add verification for the first name and last name fields on both register and profile forms * Convert username if updated from the profile form

--- Users/forms.py
+++ Users/forms.py
@@ -31,6 +31,22 @@ class UserRegisterForm(UserCreationForm):
                 f'This email address is already associated with another account.')
         return email
 
+    def clean_first_name(self):
+        '''Verify whether the input is a valid alphabetic'''
+        first_name = self.cleaned_data.get('first_name')
+        if not first_name.isalpha():
+            raise forms.ValidationError(
+                f'Are you sure that is a valid name. Names can only have alphabets')
+        return first_name
+
+    def clean_last_name(self):
+        '''Verify whether the input is alphabetic when there's one'''
+        last_name = self.cleaned_data.get('last_name')
+        if last_name and not last_name.isalpha():
+            raise forms.ValidationError(
+                f'Are you sure that is a valid name. Names can only have alphabets')
+        return last_name
+
 
 class UserUpdateForm(forms.ModelForm):
     first_name = forms.CharField(max_length=100)
@@ -40,6 +56,39 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
+
+    def clean_username(self):
+        '''Return username in lower case'''
+        return self.cleaned_data.get('username').lower()
+
+    def clean_email(self):
+        '''
+        Returns
+            email address in lower case if email is unique
+            error message in case it isn't.
+        '''
+        email = self.cleaned_data.get('email').lower()
+        username = self.cleaned_data.get('username').lower()
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError(
+                f'This email address is already associated with another account.')
+        return email
+
+    def clean_first_name(self):
+        '''Verify whether the input is a valid alphabetic'''
+        first_name = self.cleaned_data.get('first_name')
+        if not first_name.isalpha():
+            raise forms.ValidationError(
+                f'Are you sure that is a valid name. Names can only have alphabets')
+        return first_name
+
+    def clean_last_name(self):
+        '''Verify whether the input is alphabetic when there's one'''
+        last_name = self.cleaned_data.get('last_name')
+        if last_name and not last_name.isalpha():
+            raise forms.ValidationError(
+                f'Are you sure that is a valid name. Names can only have alphabets')
+        return last_name
 
 
 class ProfileUpdateForm(forms.ModelForm):
