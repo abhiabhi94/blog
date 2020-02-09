88b8c6ed23b27ef6d082488fba02e7b18881c4e0
abhiabhi94 <abhy.abhy94@gmail.com>
2020-02-09 16:29:45 +0530

    Correct output on large values

--- Blog/templatetags/cool_view.py
+++ Blog/templatetags/cool_view.py
@@ -28,4 +28,4 @@ def cool_view(val, precision=2):
     elif int_val < 1_000_000:
         return f'{ int_val/1000.0:.{precision}f}'.rstrip('0').rstrip('.') + 'K'
     else:
-        return f'{int_val/1_000_000.0:.{precision}f}'.rstrip('0.') + 'M'
+        return f'{int_val/1_000_000.0:.{precision}f}'.rstrip('0').rstrip('.') + 'M'
