ae1c4c671686d0bb6aa3f108986c168bfd13b6ae
abhiabhi94 <abhy.abhy94@gmail.com>
2020-02-16 00:56:40 +0530

    Fix issue for author's page
    Fix advertisment interfaring with share icons on detail page -- partially

--- Blog/views.py
+++ Blog/views.py
@@ -237,7 +237,7 @@ class AuthorPostListView(ListView):
     # model = Post
     template_name = 'Blog/author_posts.html'   # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-    paginate_by
+    paginate_by = 10
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
