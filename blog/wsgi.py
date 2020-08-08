9ff53f34569ea8dbdbb00d357427ca0ad608fd34
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2020-08-09 02:55:32 +0530

    Fix the modwsgi issue once and for all hopefully :)

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -8,10 +8,10 @@ class WSGIEnvironment(WSGIHandler):
 
     def __call__(self, environ, start_response):
 
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
         os.environ['EMAIL_USER'] = environ['EMAIL_USER']
         os.environ['EMAIL_PASS'] = environ['EMAIL_PASS']
         os.environ['SECRET_KEY'] = environ['SECRET_KEY']
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
         django.setup()
         return super(WSGIEnvironment, self).__call__(environ, start_response)
 
